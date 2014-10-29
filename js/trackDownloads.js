641a75f90f26b64bfb29fa48eab7e3ca60e589b2
Louis Pilfold <louis@lpil.uk>
2014-10-29 10:24:41 +0000

    Don't try to add pList tracking on non pList pages

--- js/trackDownloads.js
+++ js/trackDownloads.js
@@ -16,35 +16,37 @@ var trackOutboundLink;
   //
   pList = document.querySelector('#roiProdList_gridViewProducts');
 
-  // Find out which column is the name column
-  pListHeaders = pList.querySelectorAll('th');
+  if (pList) {
+    // Find out which column is the name column
+    pListHeaders = pList.querySelectorAll('th');
 
-  i = pListHeaders.length;
-  while (i--) {
-    if (/Name/.test(pListHeaders[i].textContent)) {
-      nameColIndex = i;
-      break;
+    i = pListHeaders.length;
+    while (i--) {
+      if (/Name/.test(pListHeaders[i].textContent)) {
+        nameColIndex = i;
+        break;
+      }
     }
-  }
 
-  // Attach the trackOutboundLink function to each preview link
-  links = pList.querySelectorAll('a');
+    // Attach the trackOutboundLink function to each preview link
+    links = pList.querySelectorAll('a');
 
-  pListOnclick = function pListOnclick() {
-    trackOutboundLink(
-        // Event name is the document name
-        this.parentElement
-          .parentElement
-          .childNodes[nameColIndex + 1]
-          .textContent
-          .trim()
-          );
-  };
+    pListOnclick = function pListOnclick() {
+      trackOutboundLink(
+          // Event name is the document name
+          this.parentElement
+            .parentElement
+            .childNodes[nameColIndex + 1]
+            .textContent
+            .trim()
+            );
+    };
 
-  i = links.length;
-  while (i--) {
-    if (links[i].pathname === '/getThumbnail.aspx') {
-      links[i].addEventListener('click', pListOnclick);
+    i = links.length;
+    while (i--) {
+      if (links[i].pathname === '/getThumbnail.aspx') {
+        links[i].addEventListener('click', pListOnclick);
+      }
     }
   }
 }());
